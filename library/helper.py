d3c831689ca0d813cef7a49f32a65002e60e8599
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2023-02-01 14:54:25 +0530

    sql handler is fixed

--- library/helper.py
+++ library/helper.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import mysql.connector
+from multiprocessing.dummy import ThreadPool
 
 class DataHandler:
     def __init__(self,columns=None,dtypes=None,is_SQL=False,SQL_handle=None,table_name=None):
@@ -15,94 +16,105 @@ class DataHandler:
         if not self.is_SQL:
             self.table=pd.DataFrame(columns=self.columns)
         else:
-            self.table_name=self.SQL_handle.hasTable(tabname=table_name,columns=self.columns,dtypes=self.dtypes)
+            self.jobrunner=ThreadPool(1)
+            self.table_name=self.jobrunner.apply(self.SQL_handle.hasTable,(table_name,self.columns,self.dtypes))
     
     @property
     def Count(self):
         if not self.is_SQL:
             return self.table.shape[0]
         else:
-            return self.SQL_handle.Count(self.table_name)
+            return self.jobrunner.apply(self.SQL_handle.Count,(self.table_name,))
 
     def Insert(self,row):
         if not self.is_SQL:
             self.table.loc[self.table.shape[0]]=row
         else:
-            self.SQL_handle.Insert(self.table_name,row)
+            self.jobrunner.apply(self.SQL_handle.Insert,(self.table_name,row))
 
     def Update(self,idx,col,val):
         if not self.is_SQL:
             self.table.loc[idx,col]=val
         else:
-            self.SQL_handle.setVal(self.table_name,idx,col,val)
+            self.jobrunner.apply(self.SQL_handle.setVal,(self.table_name,idx,col,val))
 
     def GetAT(self,idx,col):
         if not self.is_SQL:
             return self.table.loc[idx,col]
         else:
-            return self.SQL_handle.getVal(self.table_name,idx,col)
+            return self.jobrunner.apply(self.SQL_handle.getVal,(self.table_name,idx,col))
 
     def IncrementBy(self,idx,col,by):
         if not self.is_SQL:
             self.table.loc[idx,col]+=by
         else:
-            self.SQL_handle.IncrementBy(self.table_name,idx,col,by)
+            self.jobrunner.apply(self.SQL_handle.IncrementBy,(self.table_name,idx,col,by))
 
 
 class SQLHandler:
     def __init__(self,host='localhost',user='root',password='abc',db='dQdb'):
+        self.host=host
+        self.user=user
+        self.password=password
+        self.db=db
+
+    def connect(self):
         connected=False
         while not connected:
             try:
-                self.mydb = mysql.connector.connect(host=host,user=user,password=password)
+                self.mydb = mysql.connector.connect(host=self.host,user=self.user,password=self.password)
+                self.UseDB(self.db)
                 connected=True
-            except:
-                pass
-        self.mycursor = self.mydb.cursor()
-        self._setup(db)
-        
-    def _setup(self,db):
-        self.UseDB(db)
+            except Exception as e:
+                print(e)
+    
+    def query(self, sql):
+        try:
+            cursor = self.mydb.cursor()
+            cursor.execute(sql)
+        except Exception:
+            self.connect()
+            cursor = self.mydb.cursor()
+            cursor.execute(sql)
+        res=cursor.fetchall()
+        cursor.close()
+        self.mydb.commit()
+        return res
 
     def UseDB(self,dbname=None):
-        self.mycursor.execute("SHOW DATABASES")
-        if dbname not in [r[0] for r in self.mycursor]:
-            self.mycursor.execute(f"CREATE DATABASE {dbname}")
-        self.mycursor.execute(f"USE {dbname}")
+        res=self.query("SHOW DATABASES")
+        if dbname not in [r[0] for r in res]:
+            self.query(f"CREATE DATABASE {dbname}")
+        self.query(f"USE {dbname}")
 
     def DropDB(self,dbname=None):
-        self.mycursor.execute("SHOW DATABASES")
-        if dbname in [r[0] for r in self.mycursor]:
-            self.mycursor.execute(f"DROP DATABASE {dbname}")
-        self.mydb.commit()
+        res=self.query("SHOW DATABASES")
+        if dbname in [r[0] for r in res]:
+            self.query(f"DROP DATABASE {dbname}")
 
     def hasTable(self,tabname=None,columns=None,dtypes=None):
-        self.mycursor.execute("SHOW TABLES")
-        if tabname not in [r[0] for r in self.mycursor]:
+        res=self.query("SHOW TABLES")
+        if tabname not in [r[0] for r in res]:
             dmap={'int':'INT','str':'VARCHAR(32)'}
             col_config=''
             for c,d in zip(columns,dtypes):
                 col_config+=f", {c} {dmap[d]}"
-            self.mycursor.execute(f"CREATE TABLE {tabname} (id INT AUTO_INCREMENT PRIMARY KEY{col_config})")
-        self.mydb.commit()
+            self.query(f"CREATE TABLE {tabname} (id INT AUTO_INCREMENT PRIMARY KEY{col_config})")
         return tabname
 
     def getVal(self,table_name,idx,col):
-        self.mycursor.execute(f"SELECT {col} FROM {table_name} where id={idx+1}")
-        row=self.mycursor.fetchall()
+        row=self.query(f"SELECT {col} FROM {table_name} where id={idx+1}")
         if len(row)==0:raise KeyError(f"Key:idx-{idx} is not found")
         else:return row[0][0]
 
     def setVal(self,table_name,idx,col,val):
         if type(val)==str:
-            self.mycursor.execute(f"UPDATE {table_name} SET {col}='{val}' WHERE id={idx+1}")
+            self.query(f"UPDATE {table_name} SET {col}='{val}' WHERE id={idx+1}")
         else:
-            self.mycursor.execute(f"UPDATE {table_name} SET {col}={val} WHERE id={idx+1}")
-        self.mydb.commit()
+            self.query(f"UPDATE {table_name} SET {col}={val} WHERE id={idx+1}")
 
     def IncrementBy(self,table_name,idx,col,by):
-        self.mycursor.execute(f"UPDATE {table_name} SET {col}={col}+{by} WHERE id={idx+1}")
-        self.mydb.commit()
+        self.query(f"UPDATE {table_name} SET {col}={col}+{by} WHERE id={idx+1}")
 
     def Insert(self,table_name,row):
         row_str='0'
@@ -112,16 +124,14 @@ class SQLHandler:
             else:
                 v_reduced='NULL' if np.isnan(v) else v
                 row_str+=f", {v_reduced}"
-        self.mycursor.execute(f"INSERT INTO {table_name} VALUES ({row_str})")
-        self.mydb.commit()
+        self.query(f"INSERT INTO {table_name} VALUES ({row_str})")
 
     def getTopicTables(self,):
-        self.mycursor.execute("SHOW TABLES")
-        return [r[0] for r in self.mycursor if r[0] not in ['subl','publ']]
+        res=self.query("SHOW TABLES")
+        return [r[0] for r in res if r[0] not in ['subl','publ']]
     
     def Count(self,table_name):
-        self.mycursor.execute(f"SELECT count(id) FROM {table_name}")
-        res=self.mycursor.fetchall()
+        res=self.query(f"SELECT count(id) FROM {table_name}")
         return res[0][0]
     
 
