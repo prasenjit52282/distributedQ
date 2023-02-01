fd82e6b0a664b7375d219ed103668128b39ef84c
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2023-02-01 05:41:51 +0530

    create the topics in the begining of 5P3C

--- test5P3C.sh
+++ test5P3C.sh
@@ -1,6 +1,11 @@
 HOST=$1
 PORT=$2
 
+#creating topics
+curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T-1"}' -H "Content-Type: application/json"
+curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T-2"}' -H "Content-Type: application/json"
+curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T-3"}' -H "Content-Type: application/json"
+
 
 #producers
 python runproduce.py --id 1 --topics T-1 T-2 T-3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
@@ -9,8 +14,6 @@ python runproduce.py --id 3 --topics T-1 --broker  ${HOST}:${PORT} --log_loc ./t
 python runproduce.py --id 4 --topics T-2 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
 python runproduce.py --id 5 --topics T-2 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
 echo "5 producers are started"
-
-sleep 1 #sleep for 1 sec
 
 #consumers
 python runconsume.py --id 1 --topics T-1 T-2 T-3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
