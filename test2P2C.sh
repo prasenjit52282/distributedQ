11bda961c49b7ef0dc254b0b8c405e4546dde983
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2023-02-01 16:34:20 +0530

    minor changes

--- test2P2C.sh
+++ test2P2C.sh
@@ -5,15 +5,14 @@ PORT=$2
 curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T1"}' -H "Content-Type: application/json"
 curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T2"}' -H "Content-Type: application/json"
 
-#producers
-python runproduce.py --id 1 --topics T1 T2 --broker  ${HOST}:${PORT} --log_loc ./test/2P2C &
-python runproduce.py --id 2 --topics T1 T2 --broker  ${HOST}:${PORT} --log_loc ./test/2P2C &
-echo "2 producers are started"
-
-
 #consumers
 python runconsume.py --id 1 --topics T1 T2 --broker  ${HOST}:${PORT} --log_loc ./test/2P2C &
 python runconsume.py --id 2 --topics T1 T2 --broker  ${HOST}:${PORT} --log_loc ./test/2P2C &
 echo "2 consumers are started"
+
+#producers
+python runproduce.py --id 1 --topics T1 T2 --broker  ${HOST}:${PORT} --log_loc ./test/2P2C &
+python runproduce.py --id 2 --topics T1 T2 --broker  ${HOST}:${PORT} --log_loc ./test/2P2C &
+echo "2 producers are started"
 
 wait
--- test5P3C.sh
+++ test5P3C.sh
@@ -6,6 +6,11 @@ curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T1"}' -H "Conten
 curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T2"}' -H "Content-Type: application/json"
 curl -XPOST "http://${HOST}:${PORT}/topics" -d '{"topic_name": "T3"}' -H "Content-Type: application/json"
 
+#consumers
+python runconsume.py --id 1 --topics T1 T2 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
+python runconsume.py --id 2 --topics T1 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
+python runconsume.py --id 3 --topics T1 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
+echo "3 consumers are started"
 
 #producers
 python runproduce.py --id 1 --topics T1 T2 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
@@ -14,11 +19,5 @@ python runproduce.py --id 3 --topics T1 --broker  ${HOST}:${PORT} --log_loc ./te
 python runproduce.py --id 4 --topics T2 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
 python runproduce.py --id 5 --topics T2 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
 echo "5 producers are started"
-
-#consumers
-python runconsume.py --id 1 --topics T1 T2 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
-python runconsume.py --id 2 --topics T1 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
-python runconsume.py --id 3 --topics T1 T3 --broker  ${HOST}:${PORT} --log_loc ./test/5P3C &
-echo "3 consumers are started"
 
 wait
